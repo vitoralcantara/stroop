989ebbbbbf1ce6828bf780981f0e34e19115a014
Vítor Alcântara de Almeida <vit.alcantara@gmail.com>
2022-02-14 13:45:05 -0300

    atualizado interface de Nova Partida

--- lib/UI/game_config_1.dart
+++ lib/UI/game_config_1.dart
@@ -1,62 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'game_config_2.dart';
 
 class GameConfig1 extends StatelessWidget {
+  final redButtonStyle = ElevatedButton.styleFrom(
+      fixedSize: const Size(160, 60), primary: Colors.red);
+
+  final blueButtonStyle = ElevatedButton.styleFrom(
+      fixedSize: const Size(160, 60), primary: Colors.blue);
+
+  final greenButtonStyle = ElevatedButton.styleFrom(
+      fixedSize: const Size(160, 60), primary: Colors.green);
+
+  final buttonTextStyle = GoogleFonts.nunito(fontSize: 22.0);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.only(right: 10),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Text("Nova Partida"),
-                    TextButton(
-                      style: ButtonStyle(
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.blue),
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+            child: Container(
+                alignment: FractionalOffset.center,
+                margin: EdgeInsets.only(top: 40),
+                //constraints: BoxConstraints.expand(width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height),
+                child: Column(
+                    //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Spacer(
+                        flex: 2,
                       ),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => GameConfig2(),
-                            ));
-                      },
-                      child: Text('15 segundos'),
-                    ),
-                    TextButton(
-                      style: ButtonStyle(
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.blue),
+                      Text(
+                        "Nova\nPartida",
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: GoogleFonts.nunito(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 50.0,
+                            color: Colors.white),
+                        maxLines: 2,
                       ),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => GameConfig2(),
-                            ));
-                      },
-                      child: Text('30 segundos'),
-                    ),
-                    TextButton(
-                      style: ButtonStyle(
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.blue),
+                      Spacer(flex: 2),
+                      ElevatedButton(
+                        style: redButtonStyle,
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => GameConfig2(),
+                              ));
+                        },
+                        child: Text(
+                          '15 segundos',
+                          style: buttonTextStyle,
+                        ),
                       ),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => GameConfig2(),
-                            ));
-                      },
-                      child: Text('1 minuto'),
-                    )
-                  ])
-            ]));
+                      Spacer(),
+                      ElevatedButton(
+                        style: blueButtonStyle,
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => (GameConfig2()),
+                              ));
+                        },
+                        child: Text('30 segundos', style: buttonTextStyle),
+                      ),
+                      Spacer(),
+                      ElevatedButton(
+                        style: greenButtonStyle,
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => (GameConfig2()),
+                              ));
+                        },
+                        child: Text('1 minuto', style: buttonTextStyle),
+                      ),
+                      Spacer(flex: 4),
+                    ]))));
   }
 }
